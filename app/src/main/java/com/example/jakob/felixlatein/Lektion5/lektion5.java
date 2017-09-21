8d1c603522debf4a725ff3f74defaeb513aac75d
Moritz <jamoedev@gmail.com>
2017-09-21 16:20:49 +0200

    Layouts

---
+++ app/src/main/java/com/example/jakob/felixlatein/Lektion5/lektion5.java
@@ -0,0 +1,59 @@
+package com.example.jakob.felixlatein.Lektion5;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.jakob.felixlatein.R;
+import com.example.jakob.felixlatein.SectionsPageAdapter;
+
+public class lektion5 extends AppCompatActivity {
+    private ViewPager viewPager;
+    private Toolbar toolbar;
+    private SectionsPageAdapter SectionsPageAdapter3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lektion5);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Lektion 5");
+        setSupportActionBar(toolbar);
+
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        viewPager = (ViewPager)findViewById(R.id.container);
+        setupViewPager(viewPager);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        SectionsPageAdapter3 = new SectionsPageAdapter(getSupportFragmentManager());
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void setupViewPager(final ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new lektion5de(), "Deutsch");
+        adapter.addFragment(new lektion5lat(), "Latein");
+        adapter.addFragment(new lektion5voc(), "Vokabeln");
+        viewPager.setAdapter(adapter);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
