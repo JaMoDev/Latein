58cbcba7cfc1cf01737cea20b18814d84569eb8e
Moritz <jamoedev@gmail.com>
2017-11-07 17:25:56 +0100

    Lektionen

--- app/src/main/java/com/example/jakob/felixlatein/Lektion5/lektion5.java
+++ app/src/main/java/com/example/jakob/felixlatein/Lektion5/lektion5.java
@@ -52,7 +52,7 @@ public class lektion5 extends AppCompatActivity {
         if (item.getItemId()==android.R.id.home)
             finish();
         return super.onOptionsItemSelected(item);
-    }
+    } //
 
 
 
